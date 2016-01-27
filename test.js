cb70acd1c1ecfc77b9ad9e7c5ae6fe6749c49273
Yoshua Wuyts <i@yoshuawuyts.com>
2016-01-27 14:50:43 +1100

    test: add

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
-const test = require('tape')
-const virtualStreamGraph = require('./')
-
-test('should assert input types', function (t) {
-  t.plan(1)
-  t.throws(virtualStreamGraph)
-})
+// const test = require('tape')
+// const virtualStreamGraph = require('./')
